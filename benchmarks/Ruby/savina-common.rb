0c502e7c2fdfff7f117c28aee8cf60528353a70f
Benoit Daloze <eregontp@gmail.com>
2016-06-13 14:52:55 +0200

    Add the Radix Sort benchmark from the Savina benchmark suite

---
+++ benchmarks/Ruby/savina-common.rb
@@ -0,0 +1,127 @@
+# Copyright (c) 2016 Benoit Daloze
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'thread'
+
+Thread.abort_on_exception = true
+
+class Actor
+  SYNC = Mutex.new
+  ALL = []
+
+  def self.await_all_actors
+    SYNC.synchronize do
+      while thread = ALL.pop
+        thread.join
+      end
+    end
+  end
+
+  def self.inherited(subclass)
+    subclass.class_exec {
+      # avoid polymorphism by forcing specialization of these methods on the subclass
+      def send!(message)
+        @mailbox << message
+      end
+
+      def start!
+        Thread.new {
+          begin
+            while message = @mailbox.pop
+              if :exit == process(message)
+                break
+              end
+            end
+          rescue Exception => e
+            Thread.main.raise e
+          end
+        }
+      end
+    }
+  end
+
+  def self.new(*args)
+    actor = super(*args)
+    SYNC.synchronize do
+      actor.instance_variable_set :@mailbox, Queue.new
+      ALL << actor.start!
+    end
+    actor
+  end
+end
+
+class MyRandom
+  def initialize(seed)
+    @seed = seed
+  end
+
+  def next
+    @seed = ((@seed * 1309) + 13849) & 65535
+  end
+
+  def nextInt bound
+    # fair enough for low values of bound
+    self.next % bound
+  end
+end
+
+##### ARGV parsing #####
+ARGV.each_with_index { |arg, i|
+  begin
+    n = Integer(arg)
+  rescue ArgumentError
+    ARGV.shift(i)
+    break
+  end
+  ARGS[i] = n
+}
+
+##### are-we-fast-yet compatibility layer #####
+class Benchmark
+  def verify_result(result)
+    self.class.verify(result)
+  end
+end
+
+##### bench9000 compatibility layer #####
+
+if $:.find { |path| path.end_with?("bench9000/lib") }
+  class Benchmark
+    def self.inherited(subclass)
+      Object.const_set(:BENCH9000_BENCHMARK_CLASS, subclass)
+    end
+  end
+
+  def harness_input
+    BENCH9000_BENCHMARK_CLASS.new
+  end
+
+  def harness_sample(benchmark)
+    benchmark.benchmark
+  end
+
+  def harness_verify(result)
+    BENCH9000_BENCHMARK_CLASS.verify(result)
+  end
+
+  at_exit do
+    require 'bench9000/harness'
+  end
+end
